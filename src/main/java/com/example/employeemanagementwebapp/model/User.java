50a93c6646d5444a819026cff1e8c75cd50e1b96
Sefer22 <120428751+Sefer22@users.noreply.github.com>
2024-06-17 02:49:44 +0400

    developed

--- src/main/java/com/example/employeemanagementwebapp/model/User.java
+++ src/main/java/com/example/employeemanagementwebapp/model/User.java
@@ -34,6 +34,60 @@ public class User {
 
     }
 
+    public User(Long id, String firstName, String lastName, String email, String password, Collection<Role> roles) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.roles = roles;
+    }
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Collection<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Collection<Role> roles) {
+        this.roles = roles;
+    }
 }
