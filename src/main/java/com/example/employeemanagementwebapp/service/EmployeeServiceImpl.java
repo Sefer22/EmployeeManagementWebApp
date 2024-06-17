9315cd2b4d24d9e5a64ae5d92ab00ce452d6e1bc
Sefer22 <120428751+Sefer22@users.noreply.github.com>
2024-06-18 00:25:36 +0400

    developed

--- src/main/java/com/example/employeemanagementwebapp/service/EmployeeServiceImpl.java
+++ src/main/java/com/example/employeemanagementwebapp/service/EmployeeServiceImpl.java
@@ -1,12 +1,43 @@
 package com.example.employeemanagementwebapp.service;
 
+import com.example.employeemanagementwebapp.model.Employee;
 import com.example.employeemanagementwebapp.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
 
     @Autowired
     private EmployeeRepository employeeRepository;
+
+    @Override
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+    @Override
+    public void saveEmployee(Employee employee) {
+        this.employeeRepository.save(employee);
+    }
+    @Override
+    public Employee getEmployeeById(long id) {
+        Optional<Employee> optional = employeeRepository.findById(id);
+        Employee employee=null;
+        if(optional.isPresent()) {
+            employee=optional.get();
+        }else {
+            throw new RuntimeException("Employee not found for id :: "+id );
+        }
+        return employee;
+    }
+    @Override
+    public void deleteById(long id) {
+        this.employeeRepository.deleteById(id);
+    }
+
+
+
 }
