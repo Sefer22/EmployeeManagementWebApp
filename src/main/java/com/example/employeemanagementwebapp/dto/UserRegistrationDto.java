cbf5ffb81b4a7e06897d33b7554544ca60a53e3c
Sefer22 <120428751+Sefer22@users.noreply.github.com>
2024-06-18 00:50:59 +0400

    developed

--- src/main/java/com/example/employeemanagementwebapp/dto/UserRegistrationDto.java
+++ src/main/java/com/example/employeemanagementwebapp/dto/UserRegistrationDto.java
@@ -9,4 +9,12 @@ public class UserRegistrationDto {
     public UserRegistrationDto() {
 
     }
+
+    public UserRegistrationDto(String firstName, String lastName, String email, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+
 }
