b1399802cdb01be4a9da67b191d9508131f4a4dc
Sefer22 <120428751+Sefer22@users.noreply.github.com>
2024-06-18 00:42:01 +0400

    developed

--- src/main/java/com/example/employeemanagementwebapp/service/UserServiceImpl.java
+++ src/main/java/com/example/employeemanagementwebapp/service/UserServiceImpl.java
@@ -1,10 +1,15 @@
 package com.example.employeemanagementwebapp.service;
 
+import com.example.employeemanagementwebapp.model.Role;
+import com.example.employeemanagementwebapp.model.User;
 import com.example.employeemanagementwebapp.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
 
 @Service
 public class UserServiceImpl implements UserService{
@@ -20,5 +25,16 @@ public class UserServiceImpl implements UserService{
     }
 
     @Override
+    public User save(UserRegistrationDto registrationDto) {
+        User user = new User(registrationDto.getFirstName(),
+                registrationDto.getLastName(),registrationDto.getEmail(),
+                passwordEncoder.encode(registrationDto.getPassword(), Arrays.asList(new Role("ROLE_USER")));
+
+        return userRepository.save(user);
+    }
+
+    @Override
+    public UserDetails loadUser
+
 
 }
