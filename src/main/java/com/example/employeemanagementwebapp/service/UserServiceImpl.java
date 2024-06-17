45bcd56c8693e465311a632ca7942942c0ddc68e
Sefer22 <120428751+Sefer22@users.noreply.github.com>
2024-06-18 00:45:46 +0400

    developed

--- src/main/java/com/example/employeemanagementwebapp/service/UserServiceImpl.java
+++ src/main/java/com/example/employeemanagementwebapp/service/UserServiceImpl.java
@@ -4,12 +4,15 @@ import com.example.employeemanagementwebapp.model.Role;
 import com.example.employeemanagementwebapp.model.User;
 import com.example.employeemanagementwebapp.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
+import java.util.Collection;
 
 @Service
 public class UserServiceImpl implements UserService{
@@ -34,7 +37,16 @@ public class UserServiceImpl implements UserService{
     }
 
     @Override
-    public UserDetails loadUser
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username);
+        if(user==null) {
+            throw new UsernameNotFoundException("Invalid username or password");
+        }
+
+        return new org.springframework.security.core.userdetails.User(user.getEmail(),user.getPassword(),mapRolesToAuthorities(user.getRoles()));
+    }
+
+    private Collection<? extends GrantedAuthority> mapRolesToAuthorities(Collection<Role>)
 
 
 }
