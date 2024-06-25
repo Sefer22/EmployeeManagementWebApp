258a75d61cf495096cf347d34149b7b8b8f33ad3
Sefer22 <120428751+Sefer22@users.noreply.github.com>
2024-06-25 18:17:30 +0400

    developed

--- src/main/java/com/example/employeemanagementwebapp/controller/EmployeeController.java
+++ src/main/java/com/example/employeemanagementwebapp/controller/EmployeeController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -33,5 +34,16 @@ public class EmployeeController {
         return "redirect:/";
     }
     @GetMapping("/showFormForUpdate/{id}")
+    public String showFormForUpdate(@PathVariable(value = "id") Long id,Model model) {
+        Employee employee = employeeService.getEmployeeById(id);
+        model.addAttribute("employee",employee);
+        return "update_employee";
+    }
+    @GetMapping("/deleteEmployee/{id}")
+    public String deleteEmployee(@PathVariable(value = "id"),Long id) {
+        employeeService.deleteEmployeeById(id);
+        return "redirect:/";
+    }
+    @GetMapping("/page/{pageNo}")
 
 }
