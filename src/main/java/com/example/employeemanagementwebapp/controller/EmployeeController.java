964d340b5f738f772fa3e6ee99bfa76bd5413cc9
Sefer22 <120428751+Sefer22@users.noreply.github.com>
2024-06-25 18:13:25 +0400

    developed

--- src/main/java/com/example/employeemanagementwebapp/controller/EmployeeController.java
+++ src/main/java/com/example/employeemanagementwebapp/controller/EmployeeController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class EmployeeController {
@@ -24,4 +26,12 @@ public class EmployeeController {
         model.addAttribute("employee",employee);
         return "new_employee";
     }
+
+    @PostMapping("/saveEmplpyee")
+    public String saveEmployee(@ModelAttribute("employee") Employee employee) {
+        employeeService.saveEmployee(employee);
+        return "redirect:/";
+    }
+    @GetMapping("/showFormForUpdate/{id}")
+
 }
