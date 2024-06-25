1d140c3dfda71c1939350bc03c76b57bf385e6a6
Sefer22 <120428751+Sefer22@users.noreply.github.com>
2024-06-25 17:45:03 +0400

    developed

---
+++ src/main/java/com/example/employeemanagementwebapp/config/SecurityConfiguration.java
@@ -0,0 +1,35 @@
+package com.example.employeemanagementwebapp.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http
+                .authorizeHttpRequests((requests) -> requests
+                .requestMatchers("/registration**","/js/**","/css/**","/img/**").permitAll()
+                .anyRequest().authenticated()
+                )
+                .formLogin((form) ->form
+                        .loginPage()
+                )
+    }
+}
