bfc813a3c5ff0e2d2cb5a07cb3dd5467da45a97a
Sefer22 <120428751+Sefer22@users.noreply.github.com>
2024-06-25 18:34:23 +0400

    developed

--- src/main/java/com/example/employeemanagementwebapp/controller/UserRegistrationController.java
+++ src/main/java/com/example/employeemanagementwebapp/controller/UserRegistrationController.java
@@ -1,9 +1,34 @@
 package com.example.employeemanagementwebapp.controller;
 
+import com.example.employeemanagementwebapp.dto.UserRegistrationDto;
+import com.example.employeemanagementwebapp.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping
+@RequestMapping("/registration")
 public class UserRegistrationController {
+
+    @Autowired
+    private UserService userService;
+
+    @ModelAttribute("user")
+    public UserRegistrationDto userRegistrationDto() {
+        return new UserRegistrationDto();
+    }
+
+    @GetMapping
+    public String showRegistrationForm() {
+        return "registration";
+    }
+
+    @PostMapping
+    public String registerUserAccount(@ModelAttribute("user") UserRegistrationDto registrationDto) {
+        userService.save(registrationDto);
+        return "redirect:/registration?success";
+    }
 }
