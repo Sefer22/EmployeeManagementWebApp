ba62d5d4b8de03a425003f56e1da5636e94d0ef3
Sefer22 <120428751+Sefer22@users.noreply.github.com>
2024-06-25 17:50:50 +0400

    developed

--- src/main/java/com/example/employeemanagementwebapp/config/SecurityConfiguration.java
+++ src/main/java/com/example/employeemanagementwebapp/config/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.example.employeemanagementwebapp.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -29,7 +30,13 @@ public class SecurityConfiguration {
                 .anyRequest().authenticated()
                 )
                 .formLogin((form) ->form
-                        .loginPage()
-                )
+                        .loginPage("/login")
+                        .permitAll()
+                );
+        return http.build();
+    }
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
     }
 }
